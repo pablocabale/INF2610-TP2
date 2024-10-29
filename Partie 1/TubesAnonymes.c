1bd91fc717009bba89090ac4600975ef95234e3d
cabaleguerra <pablo.cabale@polymtl.ca>
2024-10-29 14:39:58 -0400

    fix inversion de fichiers

--- Partie 1/TubesAnonymes.c
+++ Partie 1/TubesAnonymes.c
@@ -4,47 +4,51 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 
 int main() {
-    mkfifo("tube1", 0600);
-    mkfifo("tube2", 0600);
-
     pid_t p3 = fork();
     if (p3 == 0) {
+        int pipe1[2];
+        int pipe2[2];
+
+        pipe(pipe1);
+        pipe(pipe2);
+
         pid_t p2 = fork();
         if (p2 == 0) {
-            pid_t p3 = fork();
-            if (p3 == 0) {
+            pid_t p1 = fork();
+            if (p1 == 0) {
                 int fd = open("In.txt", O_RDONLY);
-                int fdTube1 = open("tube1", O_WRONLY);
 
                 dup2(fd, 0);
-                dup2(fdTube1, 1);
+                dup2(pipe1[1], 1);
 
                 close(fd);
-                close(fdTube1);
+                close(pipe1[0]);
+                close(pipe1[1]);
+                close(pipe2[0]);
+                close(pipe2[1]);
 
                 execlp("rev", "rev", NULL);
             } 
 
-            int fdTube1 = open("tube1", O_RDONLY);
-            int fdTube2 = open("tube2", O_WRONLY);
+            dup2(pipe1[0], 0);
+            dup2(pipe2[1], 1);
 
-            dup2(fdTube1, 0);
-            dup2(fdTube2, 1);
-
-            close(fdTube1);
-            close(fdTube2);
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
 
             execlp("rev", "rev", NULL);
         } 
 
-        int fdTube2 = open("tube2", O_RDONLY);
+        dup2(pipe2[0], 0);  
 
-        dup2(fdTube2, 0);  
-
-        close(fdTube2);
+        close(pipe1[0]);
+        close(pipe1[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
 
         execlp("cmp", "cmp", "-", "In.txt", "-s", NULL);
     }
@@ -63,9 +67,6 @@ int main() {
     } else {
         printf("Erreur avec cmp\n");
     }
-
-    unlink("tube1");
-    unlink("tube2");
 
     return 0;
 }
